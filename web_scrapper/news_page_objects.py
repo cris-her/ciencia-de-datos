2a08caaceda650759b149d1296979f3092ed956b
Jhonbeltran <jbeltranleon@gmail.com>
2018-11-23 14:50:32 -0500

    refactorizo codigo para agregar NewsPage

--- web_scrapper/news_page_objects.py
+++ web_scrapper/news_page_objects.py
@@ -3,25 +3,14 @@ import requests
 
 from common import config
 
-class HomePage:
-     
+class NewsPage:
+    
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
 
         self._visit(url)
-    
-    @property
-    def article_links(self):
-        links_list = []
-        #Busca en la configruracion el selector de los enlaces del periodico
-        for link in self._select(self._queries['homepage_article_links']):
-            if link and link.has_attr('href'):
-                links_list.append(link)
-
-        return set(link['href'] for link in links_list)
-
 
     def _select(self, query_string):
         return self._html.select(query_string)
@@ -32,3 +21,19 @@ class HomePage:
         response.raise_for_status()
 
         self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage):
+     
+    def __init__(self, news_site_uid, url):
+        #Herencia
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        links_list = []
+        #Busca en la configruracion el selector de los enlaces del periodico
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                links_list.append(link)
+
+        return set(link['href'] for link in links_list)
