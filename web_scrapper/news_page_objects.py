42ed3d5aec68b3a5cbbd589dcbc1261e20f426ce
Jhonbeltran <jbeltranleon@gmail.com>
2018-12-07 16:37:12 -0500

    add url to csv

--- web_scrapper/news_page_objects.py
+++ web_scrapper/news_page_objects.py
@@ -9,8 +9,9 @@ class NewsPage:
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
+        self._url = url
 
-        self._visit(url)
+        self._visit(self._url)
 
     def _select(self, query_string):
         return self._html.select(query_string)
